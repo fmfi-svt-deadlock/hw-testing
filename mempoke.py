731e48b1b81e9249fc8bdd0f826c6e009559fcc3
Adam Dej <dejko.a@gmail.com>
2016-01-05 11:05:14 +0100

    Add mechanism for defining MCU control structures

--- mempoke.py
+++ mempoke.py
@@ -15,3 +15,28 @@ class DeviceMemory:
     def write(self, address, value):
         value_bytes = struct.pack('I', value)
         self.inferior.write_memory(address, value_bytes)
+
+
+def create_memory_reg(offset, name):
+    def reg_getter(self):
+        return self.device_memory.read(self.address + offset)
+
+    def reg_setter(self, value):
+        self.device_memory.write(self.address + offset, value)
+
+    return property(reg_getter, reg_setter, None, name)
+
+
+def create_mem_struct(name, registers):
+    structure_fields = {}
+
+    for register, offset in registers:
+        structure_fields[register] = create_memory_reg(offset, register)
+
+    def memory_structure_init(self, address, device_memory):
+        self.address = address
+        self.device_memory = device_memory
+
+    structure_fields['__init__'] = memory_structure_init
+
+    return type(name, (object,), structure_fields)
