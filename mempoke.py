8a28ae1c319f80e56146f1a5029222cb144d4650
Adam Dej <dejko.a@gmail.com>
2016-01-07 16:52:52 +0100

    Add mechanism for defining registers in memory mapped peripherals of MCU

--- mempoke.py
+++ mempoke.py
@@ -15,3 +15,23 @@ class DeviceMemory:
     def write(self, address, value):
         value_bytes = struct.pack('I', value)
         self.inferior.write_memory(address, value_bytes)
+
+
+class MMPeripheral(object):
+    """Memory Mapped MCU Peripheral"""
+
+    def __init__(self, address, device_memory):
+        self.device_memory = device_memory
+        self.address = address
+
+    def __getattr__(self, name):
+        if name in self.fields:
+            return self.device.read(self.address + self.fields[name])
+        else:
+            raise ValueError('This peripheral does not contain register ' + name)
+
+    def __setattr__(self, name, value):
+        if name in self.fields:
+            self.device.write(self.address + self.fields[name], value)
+        else:
+            super(MMPeripheral, self).__setattr__(name, value)
