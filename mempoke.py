60de38121112d8b2a86ce5a13e5d3d558aba528a
Adam Dej <dejko.a@gmail.com>
2016-01-10 14:06:26 +0100

    Post-review fixes (renaming / moving stuff mostly)

--- mempoke.py
+++ mempoke.py
@@ -2,25 +2,10 @@
 import struct
 
 
-class DeviceMemory:
-    def __init__(self):
-        self.inferior = gdb.selected_inferior()
-
-    def __del__(self):
-        del self.inferior
-
-    def read(self, t, address):
-        return struct.unpack(t[CODE], self.inferior.read_memory(address, 4))[0]
-
-    def write(self, type, address, value):
-        value_bytes = struct.pack(t[CODE], value)
-        self.inferior.write_memory(address, value_bytes)
-
-CODE = 0
-LENGTH = 1
-
-
 class T(object):
+    FORMAT_CHAR = 0
+    LENGTH = 1
+
     uint8_t = ('B', 1)
     uint16_t = ('H', 2)
     uint32_t = ('I', 4)
@@ -30,34 +15,51 @@ class T(object):
     int32_t = ('i', 4)
     int64_t = ('q', 8)
 
-TYPE = 0
-OFFSET = 1
+
+class DeviceMemory:
+    def __init__(self):
+        self.inferior = gdb.selected_inferior()
+
+    def __del__(self):
+        del self.inferior
+
+    def read(self, t, address):
+        return struct.unpack(t[T.FORMAT_CHAR], self.inferior.read_memory(address, 4))[0]
+
+    def write(self, type, address, value):
+        value_bytes = struct.pack(t[T.FORMAT_CHAR], value)
+        self.inferior.write_memory(address, value_bytes)
 
 
 class MMPeripheral(object):
     """Memory Mapped MCU Peripheral"""
 
+    TYPE = 0
+    OFFSET = 1
+
     compiled_fields = {}
 
-    def __init__(self, address, device_memory):
-        self.device_memory = device_memory
+    def __init__(self, address, memory):
+        self.memory = memory
         self.address = address
         offset = 0
         for t, name in self.fields:
             if name is not None:
                 self.compiled_fields[name] = (t, offset)
-            offset += t[LENGTH]
+            offset += t[T.LENGTH]
 
     def __getattr__(self, name):
         if name in self.compiled_fields:
-            return self.device_memory.read(self.compiled_fields[name][TYPE],
-                                           self.address + self.compiled_fields[name][OFFSET])
+            return self.memory.read(self.compiled_fields[name][MMPeripheral.TYPE],
+                                    (self.address +
+                                     self.compiled_fields[name][MMPeripheral.OFFSET]))
         else:
             raise ValueError('This peripheral does not contain register ' + name)
 
     def __setattr__(self, name, value):
         if name in self.compiled_fields:
-            self.device_memory.write(self.compiled_fields[name][TYPE],
-                                     self.address + self.compiled_fields[name][OFFSET], value)
+            self.memory.write(self.compiled_fields[name][MMPeripheral.TYPE],
+                              self.address + self.compiled_fields[name][MMPeripheral.OFFSET],
+                              value)
         else:
             super(MMPeripheral, self).__setattr__(name, value)
