60f88e2e90ff411f121236a0e44100ca2022f9bb
Adam Dej <dejko.a@gmail.com>
2016-01-11 16:29:08 +0100

    Use formatted strings, add tests filter

--- test_sequencer.py
+++ test_sequencer.py
@@ -1,9 +1,27 @@
+import sys
+
+# "Test" is a function. It takes no arguments and returns any encountered errors.
+# If there is no error, test should return 'None'. Tests shouldn't have any dependencies
+# amongst themselves.
+
+
 def run(tests):
-    print '=> Going to run', len(tests), 'tests'
+    """If no arguments (sys.argv) are given, runs tests. If there are any arguments they are
+       interpreted as names of tests to actually run, it will skip other tests"""
+
+    filter = set(sys.argv[1:])
+    if len(filter) > 0:
+        to_run = []
+        for test in tests:
+            if test.__name__ in filter:
+                to_run.append(test)
+        tests = to_run
+
+    print '=> Going to run {0} tests'.format(len(tests))
     ok = []
     fail = []
     for number, test in enumerate(tests):
-        print '\t-> [' + str(number) + '/' + str(len(tests)) + ']', test.__doc__
+        print '\t-> [{0}/{1}] {2} ({3})'.format(number, len(tests), test.__name__, test.__doc__)
         error = test()
         if error is None:
             ok.append((number, test))
@@ -12,20 +30,19 @@ def run(tests):
 
     print ''
     print 'RESULTS'
-    print '\tOK: ' + str(len(ok))
-    print '\tFAILED: ' + str(len(fail))
+    print '\tOK: {0}'.format(len(ok))
+    print '\tFAILED: {0}'.format(len(fail))
     if len(fail) > 0:
         print ''
         print '--- Failures ---'
         for number, test, error in fail:
-            print 'Test ' + str(number) + ' - ' + test.__name__ + ' (' + test.__doc__ + '):'
-            print str(error)
+            print 'Test {0} - {1} ({2})\n{3}'.format(number, test.__name__, test.__doc__, error)
 
 
 def ask(question):
     answer = None
     while True:
-        print '\t\t-?', question, '[Y/N]',
+        print '\t\t-? {0} [Y/N]'.format(question),
         answer = raw_input()
         if answer.strip().upper() == 'Y' or answer.strip().upper() == 'N':
             break
