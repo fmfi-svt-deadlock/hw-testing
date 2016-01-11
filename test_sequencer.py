bc521bde2cd8796d88f8c852143827873ab2d389
Adam Dej <dejko.a@gmail.com>
2016-01-11 17:58:28 +0100

    Fix code-style problems with test_sequencer (code review)

--- test_sequencer.py
+++ test_sequencer.py
@@ -6,22 +6,24 @@ import sys
 
 
 def run(tests):
-    """If no arguments (sys.argv) are given, runs tests. If there are any arguments they are
-       interpreted as names of tests to actually run, it will skip other tests"""
+    """Runs appropriate tests.
 
-    filter = set(sys.argv[1:])
-    if len(filter) > 0:
-        to_run = []
-        for test in tests:
-            if test.__name__ in filter:
-                to_run.append(test)
+    If no arguments (sys.argv) are given, runs all tests in `tests`.
+    If there are any arguments they are interpreted as names of tests to
+    actually run, it will skip other tests.
+    """
+
+    selected_tests = set(sys.argv[1:])
+    if selected_tests:
+        to_run = [t for t in tests if t.__name__ in selected_tests]
         tests = to_run
 
-    print '=> Going to run {0} tests'.format(len(tests))
+    print '=> Going to run {} tests'.format(len(tests))
     ok = []
     fail = []
     for number, test in enumerate(tests):
-        print '\t-> [{0}/{1}] {2} ({3})'.format(number, len(tests), test.__name__, test.__doc__)
+        print '\t-> [{num}/{total}] {name} ({doc})'.format(num=number, len=len(tests),
+                                                           name=test.__name__, doc=test.__doc__).
         error = test()
         if error is None:
             ok.append((number, test))
@@ -30,13 +32,14 @@ def run(tests):
 
     print ''
     print 'RESULTS'
-    print '\tOK: {0}'.format(len(ok))
-    print '\tFAILED: {0}'.format(len(fail))
+    print '\tOK: {}'.format(len(ok))
+    print '\tFAILED: {}'.format(len(fail))
     if len(fail) > 0:
         print ''
         print '--- Failures ---'
         for number, test, error in fail:
-            print 'Test {0} - {1} ({2})\n{3}'.format(number, test.__name__, test.__doc__, error)
+            print 'Test {number} - {name} ({doc})\n{err}'.format(number=number, name=test.__name__,
+                                                                 doc=test.__doc__, err=error)
 
 
 def ask(question):
@@ -44,6 +47,6 @@ def ask(question):
     while True:
         print '\t\t-? {0} [Y/N]'.format(question),
         answer = raw_input()
-        if answer.strip().upper() == 'Y' or answer.strip().upper() == 'N':
+        if answer.strip().upper() in ['Y', 'N']:
             break
-    return True if answer.strip().upper() == 'Y' else False
+    return answer.strip().upper() == 'Y'
